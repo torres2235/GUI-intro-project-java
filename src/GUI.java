195c930cbc7c205147756378db11ddae7e854baf
Torres2235 <Torres2235@gmail.com>
2021-10-13 20:09:26 -0700

    adding JButton and JLabel

--- src/GUI.java
+++ src/GUI.java
@@ -1,22 +1,31 @@
-//this import allows us to use JFrame
-import javax.swing.JFrame;
+//this import allows us to use JFrame, JPanel, BorderFactory, JButton, JLabel
+import javax.swing.*;
 
-import javax.swing.JPanel;
-
-import javax.swing.BorderFactory;
+//this import allows us to use GridLayout, BorderLayout
 import java.awt.*;
 
 public class GUI {
 
+    //constructor
     public GUI() {
 
         //the JFrame makes a window for our project
         JFrame frame = new JFrame();
 
+        //JButton adds buttons to our GUI
+        JButton button = new JButton("Click me!");
+        JLabel label = new JLabel(("Number of clicks: 0"));
+
         //
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+
+        //adds our button to our window
+        panel.add(button);
+
+        //adds our label to our window
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,6 +35,6 @@ public class GUI {
     }
     public static void main(String[] args) {
 
-
+        new GUI();
     }
 }
